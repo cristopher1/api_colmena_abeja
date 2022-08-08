6d977c45dbeac5bd1419c232ffec2798f6798a81
Cristopher Jiménez <cristopher.jimene.13@sansano.usm.cl>
2022-08-08 18:07:53 -0400

    modificación de settings.py

--- api_memoria_colmena_abejas/settings.py
+++ api_memoria_colmena_abejas/settings.py
@@ -99,6 +99,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
