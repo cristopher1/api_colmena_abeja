fc0948204846b6f2ca4a1747607089750e8de7c8
Cristopher Jiménez <cristopher.jimene.13@sansano.usm.cl>
2022-08-08 18:05:40 -0400

    manejo de excepciones

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from tensorflow import keras
 from pathlib import Path
 import librosa
-
+import logging
+logger = logging.getLogger('django')
 
 cnn_path = Path(__file__).resolve().parent / 'CNN' / \
     '13_mfcc_abeja_reina_3_seg.h5'
@@ -14,9 +15,12 @@ model = keras.models.load_model(cnn_path)
 class EstadoSaludColmena(APIView):
 
     def post(self, request):
-        audio = request.FILES['audio']
-        serie_tiempo, tasa_muestreo = librosa.load(audio, sr=None)
-        mfcc_13 = librosa.feature.mfcc(
-            y=serie_tiempo, sr=tasa_muestreo, n_mfcc=13, dct_type=2)
-        prediccion = model.predict(mfcc_13)
-        return Response(prediccion, status=status.HTTP_200_OK)
+        try:
+            audio = request.FILES['audio'].file
+            serie_tiempo, tasa_muestreo = librosa.load(audio, sr=None)
+            mfcc_13 = librosa.feature.mfcc(
+                y=serie_tiempo, sr=tasa_muestreo, n_mfcc=13, dct_type=2)
+            prediccion = model.predict(mfcc_13)
+            return Response(prediccion, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
