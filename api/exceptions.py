8020031ffb8febe808116dcaf04a6f95f2862717
Cristopher Jiménez <cristopher.jimene.13@sansano.usm.cl>
2022-09-23 18:37:28 -0300

    agregado excepciones

---
+++ api/exceptions.py
@@ -0,0 +1,10 @@
+class FileCopyError(Exception):
+    def __init__(self, message, errors):
+        super(Exception, self).__init__(message)
+        self.errors = errors
+
+
+class AudioLoadError(Exception):
+    def __init__(self, message, errors):
+        super(Exception, self).__init__(message)
+        self.errors = errors
