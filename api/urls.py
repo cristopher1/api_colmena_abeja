941d79e413d08eb01185f9a9076b62067ffe8a4f
crisjimenezmeza <crisjimenezmeza@live.com>
2023-07-15 13:12:00 -0400

    se han cambiado las urls usadas para consultar el estado de la colmena

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('abeja_reina/', views.EstadoSaludColmena.as_view()),
+    path('estado_colmena/', views.EstadoSaludColmena.as_view()),
 ]
